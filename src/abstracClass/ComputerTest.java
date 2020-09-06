11a1443a699d1979848f0edc381777bb20a300bc
LAPTOP-03186LR8\berry <a01056239778@gmail.com>
2020-09-06 13:21:07 +0900

    #1 추상클래스 구현

---
+++ src/abstracClass/ComputerTest.java
@@ -0,0 +1,11 @@
+package abstracClass;
+
+public class ComputerTest {
+    public static void main(String[] args) {
+        //Computer c1 = new Computer(); 인스턴스 생성 불가
+        Computer c2 = new DeskTop();
+        //Computer c3 = new NoteBook(); 인스턴스 생성  불가
+        Computer c4 = new MyNoteBook();
+
+    }
+}
